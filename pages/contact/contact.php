691680e68ec13f19ab4c2a9c083fcbd65ff43d65
Joki004 <101185519+Joki004@users.noreply.github.com>
2023-04-24 19:25:28 +0200

    Contact form

---
+++ pages/contact/contact.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['submit'])) {
+    $to = "info@beautybarby.pl";
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $subject = "New form submission from ".$firstName . " ID :\n" . $lastName ."\n".$firstName."\n".$phone;
+    $subject .= " " . $lastName;
+    $headers = "From: ".$email;
+    mail($to,$subject,$message,$headers);
+    echo "Thank you for your message!";
+}
+?>
